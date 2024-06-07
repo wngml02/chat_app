cb1bdb6f68b9221eae5e72cb6de0980df20aa0c5
박주희 <wngml021211@gmail.com>
2024-06-08 00:42:16 +0900

    user1,2 edit

--- user1.c
+++ user1.c
@@ -1,11 +1,11 @@
 #include "common.h"
 
 #define SERVER_MSG_QUEUE 1234
-#define CLIENT_MSG_QUEUE 5678
-#define LOG_FILE "server_log.txt"
+#define USER1_MSG_QUEUE 5678
+#define LOG_FILE "user1_log.txt"
 
-// 클라이언트 메시지를 처리하는 함수
-void handle_client_message(int client_msgid) {
+// 사용자 1 메시지를 처리하는 함수
+void handle_user1_message(int user1_msgid) {
     struct message msg;
     int log_fd = open(LOG_FILE, O_WRONLY | O_CREAT | O_APPEND, 0644);
 
@@ -15,27 +15,27 @@ void handle_client_message(int client_msgid) {
     }
 
     while (1) {
-        // 클라이언트 메시지 수신
-        if (msgrcv(client_msgid, (void *)&msg, sizeof(msg.msg_text), 0, 0) == -1) {
+        // 사용자 1 메시지 수신
+        if (msgrcv(user1_msgid, (void *)&msg, sizeof(msg.msg_text), 0, 0) == -1) {
             perror("msgrcv");
             exit(EXIT_FAILURE);
         }
 
-        // 클라이언트 메시지 출력 및 로그 파일에 저장
-        printf("Client: %s\n", msg.msg_text);
-        dprintf(log_fd, "Client: %s\n", msg.msg_text);
+        // 사용자 1 메시지 출력 및 로그 파일에 저장
+        printf("User1: %s\n", msg.msg_text);
+        dprintf(log_fd, "User1: %s\n", msg.msg_text);
 
-        // 클라이언트가 "exit" 메시지를 보낸 경우 채팅 종료
+        // 사용자가 "exit" 메시지를 보낸 경우 채팅 종료
         if (strncmp(msg.msg_text, "exit", 4) == 0) {
-            printf("Client has exited the chat.\n");
-            dprintf(log_fd, "Client has exited the chat.\n");
+            printf("User1 has exited the chat.\n");
+            dprintf(log_fd, "User1 has exited the chat.\n");
             break;
         }
 
-        // 클라이언트가 "edit" 메시지를 보낸 경우 텍스트 편집기 실행
+        // 사용자가 "edit" 메시지를 보낸 경우 텍스트 편집기 실행
         if (strncmp(msg.msg_text, "edit", 4) == 0) {
-            printf("Client requested to open the editor.\n");
-            dprintf(log_fd, "Client requested to open the editor.\n");
+            printf("User1 requested to open the editor.\n");
+            dprintf(log_fd, "User1 requested to open the editor.\n");
 
             pid_t editor_pid = fork();
             if (editor_pid == 0) {
@@ -50,28 +50,28 @@ void handle_client_message(int client_msgid) {
 }
 
 int main() {
-    int server_msgid, client_msgid;
+    int server_msgid, user1_msgid;
     struct message msg;
 
     // 메시지 큐 생성
     server_msgid = msgget((key_t)SERVER_MSG_QUEUE, 0666 | IPC_CREAT);
-    client_msgid = msgget((key_t)CLIENT_MSG_QUEUE, 0666 | IPC_CREAT);
+    user1_msgid = msgget((key_t)USER1_MSG_QUEUE, 0666 | IPC_CREAT);
 
-    if (server_msgid == -1 || client_msgid == -1) {
+    if (server_msgid == -1 || user1_msgid == -1) {
         perror("msgget");
         exit(EXIT_FAILURE);
     }
 
-    // 클라이언트 메시지를 별도의 프로세스에서 처리
+    // 사용자 1 메시지를 별도의 프로세스에서 처리
     pid_t pid = fork();
     if (pid == 0) {
-        handle_client_message(client_msgid);
+        handle_user1_message(user1_msgid);
         exit(EXIT_SUCCESS);
     }
 
     // 서버 메시지 송신 루프
     while (1) {
-        printf("Server: ");
+        printf("User2: ");
         fgets(msg.msg_text, MAX_TEXT, stdin);
 
         msg.msg_type = 1;  // 메시지 타입 설정
@@ -84,7 +84,7 @@ int main() {
 
         // 서버가 "exit" 메시지를 보낸 경우 채팅 종료
         if (strncmp(msg.msg_text, "exit", 4) == 0) {
-            printf("Server has exited the chat.\n");
+            printf("User2 has exited the chat.\n");
             break;
         }
     }
--- user2.c
+++ user2.c
@@ -1,11 +1,11 @@
 #include "common.h"
 
 #define SERVER_MSG_QUEUE 1234
-#define CLIENT_MSG_QUEUE 5678
-#define LOG_FILE "client_log.txt"
+#define USER2_MSG_QUEUE 5678
+#define LOG_FILE "user2_log.txt"
 
-// 서버 메시지를 처리하는 함수
-void handle_server_message(int server_msgid) {
+// 사용자 2 메시지를 처리하는 함수
+void handle_user2_message(int user2_msgid) {
     struct message msg;
     int log_fd = open(LOG_FILE, O_WRONLY | O_CREAT | O_APPEND, 0644);
 
@@ -15,27 +15,27 @@ void handle_server_message(int server_msgid) {
     }
 
     while (1) {
-        // 서버 메시지 수신
-        if (msgrcv(server_msgid, (void *)&msg, sizeof(msg.msg_text), 0, 0) == -1) {
+        // 사용자 2 메시지 수신
+        if (msgrcv(user2_msgid, (void *)&msg, sizeof(msg.msg_text), 0, 0) == -1) {
             perror("msgrcv");
             exit(EXIT_FAILURE);
         }
 
-        // 서버 메시지 출력 및 로그 파일에 저장
-        printf("Server: %s\n", msg.msg_text);
-        dprintf(log_fd, "Server: %s\n", msg.msg_text);
+        // 사용자 2 메시지 출력 및 로그 파일에 저장
+        printf("User2: %s\n", msg.msg_text);
+        dprintf(log_fd, "User2: %s\n", msg.msg_text);
 
-        // 서버가 "exit" 메시지를 보낸 경우 채팅 종료
+        // 사용자가 "exit" 메시지를 보낸 경우 채팅 종료
         if (strncmp(msg.msg_text, "exit", 4) == 0) {
-            printf("Server has exited the chat.\n");
-            dprintf(log_fd, "Server has exited the chat.\n");
+            printf("User2 has exited the chat.\n");
+            dprintf(log_fd, "User2 has exited the chat.\n");
             break;
         }
 
-        // 서버가 "edit" 메시지를 보낸 경우 텍스트 편집기 실행
+        // 사용자가 "edit" 메시지를 보낸 경우 텍스트 편집기 실행
         if (strncmp(msg.msg_text, "edit", 4) == 0) {
-            printf("Server requested to open the editor.\n");
-            dprintf(log_fd, "Server requested to open the editor.\n");
+            printf("User2 requested to open the editor.\n");
+            dprintf(log_fd, "User2 requested to open the editor.\n");
 
             pid_t editor_pid = fork();
             if (editor_pid == 0) {
@@ -50,41 +50,41 @@ void handle_server_message(int server_msgid) {
 }
 
 int main() {
-    int server_msgid, client_msgid;
+    int server_msgid, user2_msgid;
     struct message msg;
 
     // 메시지 큐 생성
     server_msgid = msgget((key_t)SERVER_MSG_QUEUE, 0666 | IPC_CREAT);
-    client_msgid = msgget((key_t)CLIENT_MSG_QUEUE, 0666 | IPC_CREAT);
+    user2_msgid = msgget((key_t)USER2_MSG_QUEUE, 0666 | IPC_CREAT);
 
-    if (server_msgid == -1 || client_msgid == -1) {
+    if (server_msgid == -1 || user2_msgid == -1) {
         perror("msgget");
         exit(EXIT_FAILURE);
     }
 
-    // 서버 메시지를 별도의 프로세스에서 처리
+    // 사용자 2 메시지를 별도의 프로세스에서 처리
     pid_t pid = fork();
     if (pid == 0) {
-        handle_server_message(server_msgid);
+        handle_user2_message(user2_msgid);
         exit(EXIT_SUCCESS);
     }
 
     // 클라이언트 메시지 송신 루프
     while (1) {
-        printf("Client: ");
+        printf("User1: ");
         fgets(msg.msg_text, MAX_TEXT, stdin);
 
         msg.msg_type = 1;  // 메시지 타입 설정
 
         // 클라이언트 메시지 송신
-        if (msgsnd(client_msgid, (void *)&msg, sizeof(msg.msg_text), 0) == -1) {
+        if (msgsnd(server_msgid, (void *)&msg, sizeof(msg.msg_text), 0) == -1) {
             perror("msgsnd");
             exit(EXIT_FAILURE);
         }
 
         // 클라이언트가 "exit" 메시지를 보낸 경우 채팅 종료
         if (strncmp(msg.msg_text, "exit", 4) == 0) {
-            printf("Client has exited the chat.\n");
+            printf("User1 has exited the chat.\n");
             break;
         }
     }
@@ -93,7 +93,7 @@ int main() {
     display_file_info(LOG_FILE);
 
     // 메시지 큐 삭제 및 자식 프로세스 종료 대기
-    msgctl(client_msgid, IPC_RMID, NULL);
+    msgctl(user2_msgid, IPC_RMID, NULL);
     wait(NULL);
 
     return 0;
