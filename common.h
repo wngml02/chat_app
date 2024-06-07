980985b67e1b6be91769eed9779e6923aa70e690
박주희 <wngml021211@gmail.com>
2024-06-08 00:00:28 +0900

    common edit

--- common.h
+++ common.h
@@ -10,6 +10,9 @@
 #include <sys/msg.h>
 #include <sys/wait.h>
 #include <signal.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <time.h>
 
 #define MAX_TEXT 512
 
@@ -17,5 +20,8 @@ struct message {
     long msg_type;
     char msg_text[MAX_TEXT];
 };
+
+// 파일 정보를 출력하는 함수 선언
+void display_file_info(const char *filename);
 
 #endif // COMMON_H
